e30905c0952b0d40f98d7d37fbfed4a31d9e944b
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2025-03-11 15:17:52 +0700

    video 42 full

--- src/main/java/vn/hoidanit/jobhunter/service/error/GlobalException.java
+++ src/main/java/vn/hoidanit/jobhunter/service/error/GlobalException.java
@@ -1,13 +1,20 @@
 package vn.hoidanit.jobhunter.service.error;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import vn.hoidanit.jobhunter.domain.RestResponse;
+
 @RestControllerAdvice
 public class GlobalException {
     @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handleException(IdInvalidException idException) {
-        return ResponseEntity.badRequest().body(idException.getMessage());
+    public ResponseEntity<RestResponse<Object>> handleException(IdInvalidException idException) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(idException.getMessage());
+        res.setMessage("IdInvalidException");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 }
