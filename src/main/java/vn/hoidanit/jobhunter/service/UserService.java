35a9ca4ae75c6740560e2a8170e20db12197b438
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2025-03-12 13:49:21 +0700

    video 52

--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -48,4 +48,8 @@ public class UserService {
 
         return currentUser;
     }
+
+    public User handleGetUserByUsername(String username) {
+        return this.userRepository.findByEmail(username);
+    }
 }
