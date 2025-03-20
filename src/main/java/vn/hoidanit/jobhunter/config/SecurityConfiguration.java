2774202f1c5edb6fac7dfdb8065306502e13ddbc
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2025-03-20 14:19:19 +0700

    video 124

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -45,7 +45,8 @@ public class SecurityConfiguration {
         String[] whiteList = {
                 "/",
                 "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
-                "/storage/**"
+                "/storage/**",
+                "/api/v1/email/**"
         };
 
         http
