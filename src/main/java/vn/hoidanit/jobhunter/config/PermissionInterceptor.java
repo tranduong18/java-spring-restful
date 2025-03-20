6fd94368653c640409c4bae1132eafc5978a95a9
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2025-03-20 11:36:21 +0700

    video 119

--- src/main/java/vn/hoidanit/jobhunter/config/PermissionInterceptor.java
+++ src/main/java/vn/hoidanit/jobhunter/config/PermissionInterceptor.java
@@ -14,7 +14,7 @@ import vn.hoidanit.jobhunter.domain.Role;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
-import vn.hoidanit.jobhunter.util.error.IdInvalidException;
+import vn.hoidanit.jobhunter.util.error.PermissionException;
 
 public class PermissionInterceptor implements HandlerInterceptor {
     @Autowired
@@ -46,10 +46,10 @@ public class PermissionInterceptor implements HandlerInterceptor {
                             && item.getMethod().equals(httpMethod));
 
                     if (isAllow == false) {
-                        throw new IdInvalidException("Bạn không có quyền truy cập endpoint này");
+                        throw new PermissionException("Bạn không có quyền truy cập endpoint này");
                     }
                 } else {
-                    throw new IdInvalidException("Bạn không có quyền truy cập endpoint này");
+                    throw new PermissionException("Bạn không có quyền truy cập endpoint này");
                 }
             }
         }
